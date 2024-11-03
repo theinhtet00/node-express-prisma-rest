0847bdff9db63525da70806bd584bad4dcd6c6e0
Thein Htet Oo <bitinyourheart@gmail.com>
2024-11-03 22:07:25 +0700

    express server setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
